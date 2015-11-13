55d6ef0240be9083954d325c1d37e7f5611f4fd5
Victor Felder <victorfelder@gmail.com>
2015-11-13 21:53:56 +0100

    No trailing slashes rule

--- rules/index.js
+++ rules/index.js
@@ -2,4 +2,5 @@ module.exports = {
   'no-empty-section': require('./no-empty-section'),
   'item-link': require('./item-link'),
   'blank-lines': require('./blank-lines'),
+  'resource-url': require('./resource-url'),
 };
---
+++ rules/resource-url.js
@@ -0,0 +1,18 @@
+var visit = require('unist-util-visit');
+var url = require('url');
+
+function checkURL(ast, file, preferred, done) {
+  visit(ast, 'link', function(node) {
+    var href = node.href;
+    var parsed = url.parse(node.href);
+    var target = parsed.protocol + '//' + parsed.host;
+
+    if (href === target + '/') {
+      file.warn('Remove trailing slash (' + target + ')', node);
+    }
+  });
+
+  done();
+}
+
+module.exports = checkURL;
