6f39fdb9d2bba578077ff618d1cd77659885ff14
Seth Falco <seth@falco.fun>
2021-09-27 01:55:07 +0200

    feat: allow multiple directories

--- cli.js
+++ cli.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
 const { lint } = require('./lib/lint')
+const { program } = require('commander')
 
-;((directory) => {
-  const failure = lint(directory)
-  if (failure) {
-    process.exit(1)
-  }
-  process.exit(0)
-})(process.argv[2])
+program.version('3.0.0')
+
+program
+  .command('lint', null, { isDefault: true })
+  .argument('<dirs...>')
+  .description('lint the specified directories')
+  .action((directories) => {
+    let failure
+
+    for (const directory of directories) {
+      const failed = lint(directory)
+
+      if (failed) {
+        failure = true
+      }
+    }
+
+    if (failure) {
+      process.exit(1)
+    }
+
+    process.exit(0)
+  })
+
+program.parse(process.argv)
